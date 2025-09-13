855f4afef2ea8217656917083ec9aafe147011fb
TMTMPST <vidoelag@gmail.com>
2025-09-13 12:48:50 +0700

    Fix formatting issue in nested loop condition in praktikum3.dart

--- Week-03/src/praktikum3.dart
+++ Week-03/src/praktikum3.dart
@@ -1,7 +1,7 @@
 void main() {
   for (int index = 10; index < 27; index++) {
     for (int index = 10; index < 27; index++) {
-      if (index == 21) {Í
+      if (index == 21) {
         break;
       } else if (index > 1 && index < 7) {
         continue;
