1f8616cdc80e34f0b56ab76be32325494c01ed09
TMTMPST <vidoelag@gmail.com>
2025-10-27 01:13:04 +0700

    sadasd

---
+++ Week-09/kamera_filter/lib/widget/filter_item.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+@immutable
+class FilterItem extends StatelessWidget {
+  final String? imgPath;
+  const FilterItem({
+    super.key,
+    required this.color,
+    this.onFilterSelected,
+    this.imgPath,
+  });
+
+  final Color color;
+  final VoidCallback? onFilterSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onFilterSelected,
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ClipOval(
+            child: imgPath != null
+                ? Image.file(
+                    File(imgPath!),
+                    color: color.withOpacity(0.5),
+                    colorBlendMode: BlendMode.hardLight,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        color: Colors.grey,
+                        child: const Icon(Icons.error, color: Colors.white),
+                      );
+                    },
+                  )
+                : Image.network(
+                    'https://me-2025.vercel.app/hz-background-dark.webp',
+                    color: color.withOpacity(0.5),
+                    colorBlendMode: BlendMode.hardLight,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        color: Colors.grey,
+                        child: const Icon(Icons.error, color: Colors.white),
+                      );
+                    },
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+}
